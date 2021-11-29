33382ddf9296c26392dd27b4645d5c68685fd884
Julius Loges <webdesign@julius-loges.de>
2021-11-29 17:46:24 +0100

    Initial Commit

---
+++ github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github
+//
+//  Created by Julius Loges on 29.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Julius Loges on 29.11.21.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
